a6c2cfeae86bcefac62b0b4888fe15090e2e579c
Sergey <univ.silence@gmail.com>
2020-02-02 13:58:37 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -1,47 +1,93 @@
+'use strict';
+
+var OFFER_COUNT = 8;
+var MAP_WIDTH = 1200;
+var PIN_SIZE = 65;
+
 var OFFER_TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var CHECK_TIME = ['12:00', '13:00', '14:00'];
 var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-var ads = [];
+var offers = [];
+
+var map = document.querySelector('.map');
+var pinTemplate = document.querySelector('#pin')
+  .content
+  .querySelector('map__pin');
 
 /**
  * Определяем случайное целое число
  * @param {number} max - Максимальное целое число
  * @return {number} Случайное целое число
  */
-var randomNumber = function (max) {
-    return Math.round(Math.random() + max);
+var randomNumber = function (min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+var locationX = randomNumber(0, MAP_WIDTH);
+var locationY = randomNumber(130, 630);
+
+/**
+ * Создаем и добавляем в массив элемент-объявление
+ * @return {void}
+ */
+var createOffers = function () {
+  for (var i = 0; i < OFFER_COUNT; i++) {
+    offers.push({
+      'author': {
+        'avatar': 'img/avatars/0' + i + '.png'
+      },
+      'offer': {
+        'title': 'заголовок предложения ' + i,
+        'address': locationX + ',' + locationY,
+        'price': [randomNumber(0, 9000)],
+        'type': OFFER_TYPE[randomNumber(0, 3)],
+        'rooms': randomNumber(1, 4),
+        'guests': randomNumber(1, 5),
+        'checkin': CHECK_TIME[randomNumber(0, 2)],
+        'checkout': CHECK_TIME[randomNumber(0, 2)],
+        'features': OFFER_FEATURES[randomNumber(0, 5)],
+        'description': "строка с описанием",
+        'photos': OFFER_PHOTOS[randomNumber(0, 2)]
+      },
+      'location': {
+        'x': locationX,
+        'y': locationY
+      }
+    })
+  }
 };
 
 /**
- * Создаём массив из 8 объектов-объявлений
- * @return {void}
+ * Копируем вёрстку метки объявления из шаблона
+ * @return {object} Возвращаем объект-вёрстку объявления (метка на карте)
  */
-var createAds = function () {
-    ads.push(
-        {
-        'author': {
-        'avatar': 'img/avatars/0' + randomNumber(8) + '.png' 'Адреса изображений не повторяются'
-        },
-        'offer': {
-        'title': 'Объявление',
-        'address': строка, адрес предложения. Для простоты пусть пока представляет собой запись вида '{{location.x}}, {{location.y}}', например, '600, 350',
-        'price': [randomNumber(10000)],
-        'type': OFFER_TYPE[randomNumber(3)],
-        'rooms': randomNumber(4),
-        'guests': randomNumber(5),
-        'checkin': CHECK_TIME[randomNumber(2)],
-        'checkout': CHECK_TIME[randomNumber(2)],
-        'features': OFFER_FEATURES[randomNumber(5)],
-        'description': "строка с описанием",
-        'photos': OFFER_PHOTOS[2]
-        },
-    
-        'location': {
-        'x': случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-        'y': случайное число, координата y метки на карте от 130 до 630.
-        }
-    }
-    )
-}
+var renderOffer = function (item) {
+  var offerElement = pinTemplate.cloneNode(true);
+  var offerImage = offerElement.querySelector('img');
+
+  offerElement.style.left = locationX + PIN_SIZE + 'px';
+  offerElement.style.top = locationY + PIN_SIZE + 'px';
+
+  offerImage.style.src = item.author.avatar;
+  offerImage.style.alt = item.offer.title;
+
+  return offerElement;
+};
+
+/**
+ * Добавляем объявление одно за другим к вёрстке
+ * @return {void} 
+ */
+var offersAdd = function () {
+  var fragment = document.createDocumentFragment();
+
+  offers.forEach(function (item) {
+    fragment.appendChild(renderOffer(item));
+  })
+};
+
+map.classList.remove('map--faded');
+createOffers();
+offersAdd();
